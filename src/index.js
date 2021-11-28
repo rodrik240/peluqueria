28f4c148c866b9f0064f364b17974c5dd30bc043
Miguel Moreno <juanmiguelvasquezmoreno@gmail.com>
2021-11-27 23:29:30 -0500

    Implementación de plantilla

--- src/index.js
+++ src/index.js
@@ -2,14 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Login from './Login';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Registro from './Registro';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Router>
+      <Routes>
+        <Route exact path="/login" element={<Login/>} />
+        <Route path="/" element={<App/>} />
+        <Route path="/register" element={<Registro/>} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+  ,
   document.getElementById('root')
-);
+  );
+  
+
+  
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
